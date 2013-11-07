fbc32f705db259563826a45e16b1cbf4f2ee2301
Bob Stine <stine@wharton.upenn.edu>
2013-11-07 15:48:17 -0500

    change input style to use separate files

--- seq_regression.cc
+++ seq_regression.cc
@@ -1,16 +1,16 @@
 /*
   This application...
 
-  -- reads a tab delimited data file that begins with the
-  names of the variables, then
-              nRows of data and nCols of data
-  The number of rows does not count the leading line of
-  variable names.
+  -- reads 3 tab delimited data files that begin with the names of the
+  variables, then have the indicated number of data rows and columns.
+  The number of rows does not count the leading line of variable
+  names.
 
   -- fits a sequence of regression models, with Y as the first
-  variable read, followed by each of the predictors, then
+  variable read, followed the initializers, then by each of the
+  predictors in X.
 
-  -- writes the sequence r2 obtained
+  -- writes the sequence r2 obtained by the Xs
   
 */
 
@@ -35,80 +35,110 @@ typedef Eigen::MatrixXd Matrix;
 
 
 void
-parse_arguments(int argc, char** argv,	int &nRow, int &nCol, string &inputFileName, int &initModelSize, string &outputFileName);
+parse_arguments(int argc, char** argv,	int &n,
+		int &ny, string &yFileName,
+		int &ni, string &iFileName,
+		int &nx, string &xFileName, string &outputFileName);
 
 void
-fit_models(std::istream& input, int initSize, std::ostream& output);
+fit_models(int n, int ny, std::istream& yStream,
+	   int ni, std::istream &iStream,
+	   int nx, std::istream &xStream, std::ostream& output);
 
 
 int main(int argc, char** argv)
 {
   // read input options
-  int    nRows           ( 0  );
-  int    nCols           ( 0  );
-  string inputFileName   ( "" ); 
-  string outputFileName  ( "" ); 
-  int    initModelSize   (  0 );
-  parse_arguments(argc, argv, nRows, nCols, inputFileName, initModelSize, outputFileName);
-  std::clog << "MAIN: seq_regressor --nrows=" << nRows << " --ncols=" << nCols << " --input_file="
-	    << inputFileName << " --init_size=" << initModelSize << " --output_file_name=" << outputFileName << endl;
+  int    n         ( 0  );
+  int    ny        ( 2  );    // Y, m
+  int    ni        ( 0  );
+  int    nx        ( 0  );
+  string yFileName ( "" ); 
+  string iFileName ( "" );
+  string xFileName ( "" );
+  string outputFileName("");
+
+  parse_arguments(argc, argv, n, ny, yFileName, ni, iFileName, nx, xFileName, outputFileName);
+  std::clog << "MAIN: seq_regressor --n=" << n
+	    << " --ny=" << ny << " --y_file=" << yFileName 
+	    << " --ni=" << ni << " --i_file=" << iFileName
+            << " --nx=" << nx << " --x_file=" << xFileName
+	    << " --output_file_name=" << outputFileName << endl;
+  // assume all but the iFile must be named
+  std::ifstream yStream (yFileName);
+  std::ifstream xStream (xFileName);
   std::ofstream output(outputFileName);
-  if (!output)
-  { std::clog << "MAIN: Could not open file " << outputFileName << " for writing r2 sequence.\n";
+  if (0 == (n * ny * ni * nx))
+  { std::cerr << "MAIN: Illegal length, n=" << n << "  ny=" << ny << "  ni=" << ni << "  nx=" << nx << endl;
     return 0;
   }
-  if (inputFileName.size() > 0)
-  { std::ifstream input(inputFileName);
-    if (!input)
-    { std::clot << "MAIN: Could not open file " << inputFileName << " for reading input.\n";
+  if ((!yStream) || (!xStream) || (!output))
+  { std::clog << "MAIN: Could not open required file for writing r2 sequence.\n";
+    return 0;
+  }
+  if (iFileName.size() > 0)
+  { std::ifstream iStream(iFileName);
+    if (!iStream)
+    { std::clot << "MAIN: Could not open file " << iFileName << " for reading initialization data.\n";
       return 0;
     }
-    fit_models(input, nRows, nCols, initModelSize, output);
+    fit_models(n, ny, yStream, ni, iStream , nx, xStream, output);
   }
-  else fit_models(std::cin, nRows, nCols, initModelSize, output);
+  else fit_models(n, ny, yStream, ni, std::cin, nx, xStream, output);
 }
 
 void
-fit_models(std::istream &input, int n, int nCols, int initModelSize, std::ostream &output)
+fit_models(int n, int ny, std::istream& yStream,
+	   int ni, std::istream &iStream,
+	   int nx, std::istream &xStream, std::ostream& output);
 {
   output << "Name  r2  RSS AICc\n";
-  string              yName;
-  std::vector<string> iNames;
-  std::vector<string> xNames;
-  // read variable names
-  input >> yName;
-  for(int j=0; j<initModelSize; ++j)
+  // read Y data
+  std::vector<string> yNames;
+  Matrix Y(n,ny);
+  for(int j=0; j<ny; ++j)
   { string name;
-    input >> name;
+    yStream >> name;
+    yNames.push_back(name);
+  }
+  for(int i = 0; i<n; ++i)
+    for(int j=0; j<ny; ++j)
+      yStream >> Y(i,j);
+  // read initialization data
+  std::vector<string> iNames;
+  Matrix Xi(n,ni);
+  for(int j=0; j<ni; ++j)
+  { string name;
+    iStream >> name;
     iNames.push_back(name);
   }
-  for(int j=0; j<k; ++j)
+  for(int i=0; i<n; ++i)
+    for(int j=0; j<ni; ++j)
+      iStream >> Xi(i,j);
+  // read sequence of predictors
+  std::vector<string> xNames;
+  Matrix X(n,ni);
+  for(int j=0; j<nx; ++j)
   { string name;
-    input >> name;
+    xStream >> name;
     xNames.push_back(name);
   }
-  // read numerical values
-  Vector Y(n);
-  Matrix Z(n,         initModelSize    );
-  Matrix X(n, nCols - initModelSize - 1);
-  for(int i = 0; i < n; ++i)
-  { input >> Y(i);
-    for (int j=0; j<Z.cols(); ++j)
-      input >> Z(i,j);
-    for (int j=0; j<k; ++j)
-      input >> X(i,j);
-  }
+  for(int i=0; i<n; ++i)
+    for(int j=0; j<nx; ++j)
+      xStream >> X(i,j);
   // fit initial model
   const int blockSize = 0;
-  LinearRegression regr(yName, Y, blockSize);
+  LinearRegression regr(yName, Y.col(0), blockSize);
   if (initModelSize > 0)
-  { std::clog << "MAIN: Calculate initial model, fitting " << initModelSize << " regressors to response " << yName << endl;
-    FStatistic f=regr.f_test_predictors(iNames, Z);
+  { std::clog << "MAIN: Calculate initial model, fitting " << 1+ni << " regressors to response " << yName << endl;
+    FStatistic f = regr.f_test_predictor("nTokens",Y.col(1));
+    if(f.f_stat() > 0.0001) regr.add_predictors();
+    f = regr.f_test_predictors(iNames, Xi);
     if(f.f_stat() > 0.0001) regr.add_predictors();
     else std::clog << "MAIN: F = " << f.f_stat() << " initial regressors (near) singular and skipped.\n";
   }
-  std::clog << "MAIN: Calculation loop; fitting " << k << " regressors to response " << yName << endl;
-  for (int j=0; j<k; ++j)
+  std::clog << "MAIN: Calculation loop; fitting " << nx << " regressors to response " << yName << endl;
+  for (int j=0; j<nx; ++j)
   { FStatistic f=regr.f_test_predictor(xNames[j], X.col(j));
     if(f.f_stat() > 0.0001) regr.add_predictors();
     else std::clog << "MAIN: F = " << f.f_stat() << " for " << xNames[j] << " is (near) singular in sequence r2 and skipped.\n";
@@ -120,26 +150,35 @@ fit_models(std::istream &input, int n, int nCols, int initModelSize, std::ostrea
 
 
 void
-parse_arguments(int argc, char** argv, int &rows, int&cols, string &inputFile, int &initModelSize, string &outputFile)
+parse_arguments(int argc, char** argv, int &n,
+		int &ny, string &yFileName,
+		int &ni, string &iFileName,
+		int &nx, string &xFileName, string &outputFileName)
 {
   static struct option long_options[] = {
-    {"nrows",         required_argument, 0, 'r'},
-    {"ncols",         required_argument, 0, 'c'},
-    {"input_file",    required_argument, 0, 'i'},
-    {"init_size",     required_argument, 0, 'k'},
+    {"n",             required_argument, 0, 'n'},
+    {"n_y",           required_argument, 0, 'y'},
+    {"y_file",        required_argument, 0, 'Y'},
+    {"n_i",           required_argument, 0, 'i'},
+    {"i_file",        required_argument, 0, 'I'},
+    {"n_x",           required_argument, 0, 'x'},
+    {"x_file",        required_argument, 0, 'X'},
     {"output_file",   required_argument, 0, 'o'},
     {0, 0, 0, 0}                             // terminator 
   };
   int key;
   int option_index = 0;
-  while (-1 !=(key = getopt_long (argc, argv, "r:c:i:k:o:", long_options, &option_index))) // colon means has argument
+  while (-1 !=(key = getopt_long (argc, argv, "n:y:Y:i:I:x:X:o:", long_options, &option_index))) // colon means has argument
   {
     switch (key)
     {
-    case 'r' : { rows           = read_utils::lexical_cast<int>(optarg);      break; }
-    case 'c' : { cols           = read_utils::lexical_cast<int>(optarg);      break; }
-    case 'i' : { inputFile      = optarg;                                     break; }
-    case 'k' : { initModelSize  = read_utils::lexical_cast<int>(optarg);      break; }
+    case 'n' : { n           = read_utils::lexical_cast<int>(optarg);      break; }
+    case 'y' : { ny          = read_utils::lexical_cast<int>(optarg);      break; }
+    case 'Y' : { yFileName   = optarg;                                     break; }
+    case 'i' : { ni          = read_utils::lexical_cast<int>(optarg);      break; }
+    case 'I' : { iFileName   = optarg;                                     break; }
+    case 'x' : { nx          = read_utils::lexical_cast<int>(optarg);      break; }
+    case 'X' : { xFileName   = optarg;                                     break; }
     case 'o' : { outputFile     = optarg;                                     break; }
     default  : { std::cout << "PARSE: Option not recognized; returning.\n";       }
     } // switch
