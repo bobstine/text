8684f303c33f7abf8a1d25b080f41d2096d5c68a
Bob Stine <stine@wharton.upenn.edu>
2014-01-19 16:06:20 -0500

    update R file

--- lsa.R
+++ lsa.R
@@ -6,111 +6,14 @@ reset <- function() {
 	}
 
 
-##################################################################################
-# Simulate data
-##################################################################################
-
-vocab.size <- 50
-n.tokens <- 1000
-n.docs   <- 20
-doc.len  <- n.tokens/n.docs
-
-tokens <- sample(1:vocab.size, n.tokens, replace=TRUE)
-docs   <- 1+ floor( (0:(n.tokens-1))/doc.len)
-
-
-W <- matrix(0, nrow=n.tokens, ncol=vocab.size)
-for(i in 1:nrow(W)) W[i,tokens[i]] <- 1
-
-W0 <- W
-W1 <- matrix(0, nrow=n.tokens, ncol=vocab.size)
-W1[1:n.tokens-1,] <- W0[2:n.tokens,]
-
-
-D <- matrix(0, nrow=n.tokens, ncol=n.docs)
-for(i in 1:nrow(D)) D[i,docs[i]] <- 1
-
 
 ##################################################################################
-# Compare CCA to eigenvectors and SVD
-##################################################################################
-
-cca <- cancor (W0, W1, xcenter=FALSE, ycenter=FALSE); dim(cca$xcoef)
-
-	B01 <- t(W0) %*% W1
-	B10 <- t(B01)
-
-	F0  <- apply(W0, 2, sum)     
-	F0i <- 1/F0
-	F1  <- apply(W1, 2, sum)                
-	F1i <- 1/F1
-
-ev  <- eigen( diag(F0i) %*% B01 %*% diag(F1i) %*% B10 )
-
-	H0i <- 1/sqrt(F0)
-	H1i <- 1/sqrt(F1)
-
-udv <- svd(diag(H0i) %*% B01 %*% diag(H1i) )
-
-
-# --- evalues correspond to squared can cor; evectors perfectly corr
-cca$cor[1:5]^2
-ev$values[1:5]
-
-j <- 2; plot(cca$xcoef[,j], ev$vectors[,j]); cor(cca$xcoef[,j], ev$vectors[,j])
-
-
-# --- singular values match can cor; rescaled singular vectors perfect corr
-cca$cor[1:5]
-udv$d[1:5]
-
-H <- diag(H0i)
-j <- 2; plot(cca$xcoef[,j], H %*% udv$u[,j]); cor(cca$xcoef[,j], H %*% udv$u[,j])
-
-
-
-##################################################################################
-# Centroids of LSA = left evectors of SVD
-##################################################################################
-
-# lsa formed from left singular vectors of D'W, or right from W'D
-udv <- svd(t(W) %*% D)
-
-lsa <- udv$v
-
-# alternatively form centroids of documents from left singular vectors (mean shifts coefs)
-u      <- udv$u
-center <- matrix(0, n.docs, ncol(u))
-for(i in 1:n.docs) center[i,] <- apply(u[tokens[which(docs==i)],],2,sum)
-
-# similarity: if docs don't have the same length, then you need to put that scaling into
-# D matrix... otherwise you lose the linearity.
-
-par(mfrow=c(2,2))
-	j <- 1; plot(x<-lsa[,j], y<-center[,j]); abline(r<-lm(y~x),col="red"); round(coefficients(r)[2],2)
-	j <- 2; plot(x<-lsa[,j], y<-center[,j]); abline(r<-lm(y~x),col="red"); round(coefficients(r)[2],2)
-	j <- 3; plot(x<-lsa[,j], y<-center[,j]); abline(r<-lm(y~x),col="red"); round(coefficients(r)[2],2)
-	j <- 4; plot(x<-lsa[,j], y<-center[,j]); abline(r<-lm(y~x),col="red"); round(coefficients(r)[2],2)
-reset()
-
-b <- rep(0,length(udv$d))
-for(j in 1:length(b)) {x<-lsa[,j]; y<-center[,j]; b[j] <- coefficients(lm(y~x))[2]}
-plot(b,udv$d); abline(r<-lm(udv$d ~ b), col="red"); coefficients(r)
-
-cca <- cancor(lsa,center)
-
-cca$cor
-round(cca$xcoef,2)
-
-
-
-##################################################################################
-# Unified SVD results
+# linear lsa results
 ##################################################################################
 
 path <- "/Users/bob/C/text/text_src/temp/ChicagoOld3/"
-YM <- as.matrix(read.table( paste(path,"unified_y_m.txt",sep=""), header=TRUE, as.is=TRUE))
-X  <- as.matrix(read.table( paste(path,"unified_svd_1500.txt",sep=""), header=TRUE, as.is=TRUE))
+YM <- as.matrix(read.table( paste(path,"lsa_ym.txt",sep=""), header=TRUE, as.is=TRUE))
+X  <- as.matrix(read.table( paste(path,"lsa_1500.txt",sep=""), header=TRUE, as.is=TRUE))
 
 
 Y <-  YM[,1]    # log price
