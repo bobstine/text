e0f266b7bcc4d8e32d398b7bd9b76574032a199c
Bob Stine <stine@wharton.upenn.edu>
2014-10-31 11:25:06 -0400

    added shell script

---
+++ get_cv.sh
@@ -0,0 +1,5 @@
+scp hilbert:C/text/text_src/temp/ChicagoOld3/cv_15242/aic_lsa_20f.txt cv_15242/
+scp hilbert:C/text/text_src/temp/ChicagoOld3/cv_24387/aic_lsa_20f.txt cv_24387/
+scp hilbert:C/text/text_src/temp/ChicagoOld3/cv_31427/aic_lsa_20f.txt cv_31427/
+scp hilbert:C/text/text_src/temp/ChicagoOld3/cv_53853/aic_lsa_20f.txt cv_53853/
+scp hilbert:C/text/text_src/temp/ChicagoOld3/cv_73241/aic_lsa_20f.txt cv_73241/
