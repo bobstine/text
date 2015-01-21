ed8783a1c3f25852599b5ad24665a52bb25e418d
Bob Stine <stine@wharton.upenn.edu>
2015-01-21 14:14:01 -0500

    embed_auction builds data, tested to n=1000

--- prep_error/embed_auction.cc
+++ prep_error/embed_auction.cc
@@ -37,6 +37,11 @@
 #include <iterator>
 
 
+typedef std::map<std::string, std::vector<float>> EigenDictionary;
+
+EigenDictionary
+make_eigen_dictionary(std::string filename, int dim, std::set<std::string> const& vocabulary);
+
 void
 parse_arguments(int argc, char** argv,
 		std::string& vocabularyFileName,
@@ -74,10 +79,13 @@ int main(int argc, char** argv)
   string vocabFileName ("vocabulary.txt");
   string eigenFileName ("eigenwords.test");
   int    nEigenDim     (0);                 // use all that are found
-  string outputDir     ("data_dir");
+  string outputDir     ("data_dir/");
 
   parse_arguments(argc, argv, vocabFileName, eigenFileName, nEigenDim, outputDir);
-
+  if (outputDir[outputDir.size()-1]!='/') outputDir += "/";
+  std::clog << "embed_auction --vocab_file=" << vocabFileName << " --eigen_file=" << eigenFileName
+	    << " --eigen_dim=" << nEigenDim << " --output_dir=" << outputDir << std::endl;
+  
   // read vocabulary
   set<string> vocabulary;
   {
@@ -94,63 +102,8 @@ int main(int argc, char** argv)
     std::clog << "MAIN: Read vocabulary of " << vocabulary.size() << " tokens\n";
   }
 
-  // build dictionary for this vocab from eigenwords  (Paramveer's is space delimited, *UNKNOWN* first)
-  // ASSUME coordinates for OOV come first
-  map<string, vector<float>> dictionary;
-  bool needToFlushEigenStream = false;
-  {
-    std::ifstream eigenStream (eigenFileName.c_str(), std::ifstream::in);
-    if (eigenStream.fail())
-    { std::cerr << "ERROR: Eigen dictionary file `" << eigenFileName << "' not found; terminating.\n";
-      return 0;
-    }
-    std::clog << "MAIN: Starting to build dictionary with initial dim " << nEigenDim << " from file " << eigenFileName << std::endl;
-    { // special handling for first line, assumed to define OOV coordinates
-      vector<float> oov;
-      string token;
-      eigenStream >> token;  // toss file label for OOV; use "OOV"
-      string line;
-      std::getline(eigenStream, line);
-      std::istringstream ss(line);
-      float x;
-      while (ss >> x) oov.push_back(x);
-      if (nEigenDim == 0)
-      { nEigenDim = (int) oov.size();
-	if (verbose) std::clog << "Found eigen dimension = " << oov.size() << " based on found size for OOV.\n";
-	needToFlushEigenStream = false;
-      }
-      else
-      { if (nEigenDim < (int)oov.size())
-	{ oov.resize(nEigenDim);
-	  needToFlushEigenStream = true;
-	  if (verbose) std::clog << "       Will need to flush trailing elements in eigen stream.\n";
-	}
-	else
-	{ std::cerr << "ERROR: Found eigen dimension " << oov.size() << " which is smaller than requested " << nEigenDim << std::endl;
-	  return 0;
-	}
-      }
-      dictionary["OOV"] = oov;
-    }
-    while(!eigenStream.eof())
-    { string token;
-      string junk;
-      eigenStream >> token;
-      if(token.size() == 0) break;       // file has trailing blank line
-      std::transform(token.begin(), token.end(), token.begin(), ::tolower);
-      if(vocabulary.count(token) != 0)   // word type found in vocabulary
-      { std::vector<float> coor(nEigenDim);
-	for(int i=0; i<nEigenDim; ++i)
-	  eigenStream >> coor[i];
-	if (needToFlushEigenStream) std::getline(eigenStream, junk);
-	dictionary[token] = coor;        // over-writes if token present
-	if (verbose) std::clog << "      Added dictionary coor for token " << token << std::endl;
-      }
-      else // flush rest of line
-	std::getline(eigenStream, junk);
-    }
-  }
-  std::clog << "      Completed eigenword dictionary of size " << dictionary.size() << std::endl;
+  // build dictionary and find words that are missing
+  EigenDictionary dictionary = make_eigen_dictionary(eigenFileName, nEigenDim, vocabulary);
   if (dictionary.size() < vocabulary.size())
   { std::clog << "      Dictionary coordinates not found for the following " << vocabulary.size()-dictionary.size() << " tokens:\n";
     for(auto x : vocabulary)
@@ -160,9 +113,9 @@ int main(int argc, char** argv)
     std::clog << std::endl;
   }
 
-  // process each bundle of eigen coordinates to std output; use header line to name bundles
+  // process each bundle of eigen coordinates; use header line to name bundles
   string responseName;
-  vector<string> bundleNames;             
+  vector<string> bundleNames;
   if (!std::cin.eof())
   { std::cin >> responseName;       
     string headerLine;
@@ -172,15 +125,16 @@ int main(int argc, char** argv)
     while (ss >> name)
       bundleNames.push_back(name);
   }
-  std::clog << "MAIN: Reading response and " << bundleNames.size() << " words to define eigen coordinates.\n";
+  const int nBundles = (int) bundleNames.size();
+  std::clog << "MAIN: Reading response and " << nBundles << " words for each case to define modeling data.\n";
   std::vector<             string>   response;
-  std::vector< std::vector<string> > theWords(bundleNames.size());
+  std::vector< std::vector<string> > theWords(nBundles);
   while (!std::cin.eof())
   { string thePrep;
     std::cin >> thePrep;
     if (thePrep.size() == 0) break;
     response.push_back(thePrep);
-    for(int j=0; j<(int)bundleNames.size(); ++j)
+    for(int j=0; j<nBundles; ++j)
     { string token;
       std::cin >> token;
       theWords[j].push_back(token);
@@ -188,8 +142,16 @@ int main(int argc, char** argv)
   }
   std::clog << "MAIN: Read " << response.size() << " cases for response and " << theWords[0].size() << " words for first predictor.\n";
   std::clog << "MAIN: Embedding " << bundleNames.size() << " blocks of eigen coordinates.\n";
+  std::ofstream shellFile (outputDir + "index.sh");
+  if (!shellFile.good())
+  { std::cerr << "ERROR: Could not place index file in directory " << outputDir << "; exiting.\n";
+    return 0;
+  }
+  shellFile << "#!/bin/sh" << std::endl
+	    << "echo " << response.size() << std::endl
+	    << "cat " << responseName << std::endl;
   {
-    std::ofstream file (outputDir + "response");
+    std::ofstream file (outputDir + responseName);
     file << responseName << std::endl;
     file << "role y" << std::endl;
     file << response << std::endl;
@@ -197,13 +159,15 @@ int main(int argc, char** argv)
   {
     // for each bundle, open multiple files, one for each eigen dim
     int n = (int)response.size();
-    for(int bundle=0; bundle<(int)bundleNames.size(); ++bundle)
+    for(int bundle=0; bundle<nBundles; ++bundle)
     { std::clog << "+++ opening files for bundle " << bundle << std::endl;
       std::vector<std::ofstream *> files;
       for(int d=0; d<nEigenDim; ++d)
-      { std::ofstream *file = new std::ofstream(outputDir + bundleNames[bundle] + "_" + std::to_string(d));
-	(*file) << bundleNames[d] + "_" + std::to_string(d) << std::endl;
-	(*file) << "role x stream " << bundleNames[bundle] << std::endl;
+      { string varName = bundleNames[bundle] + "_" + std::to_string(d);
+	shellFile << "cat " << varName << std::endl;
+	std::ofstream *file = new std::ofstream(outputDir + varName);
+	(*file) << bundleNames[d] + "_" + std::to_string(d) << std::endl;   // name
+	(*file) << "role x stream " << bundleNames[bundle] << std::endl;    // attributes
 	files.push_back(file);
       }
       std::clog << "+++ writing coordinates for bundle " << bundle << std::endl;
@@ -227,6 +191,75 @@ int main(int argc, char** argv)
   }
 }
 
+//     make_eigen_dictionary     make_eigen_dictionary     make_eigen_dictionary     make_eigen_dictionary     make_eigen_dictionary
+/*
+  build dictionary for this vocab from eigenwords  (Paramveer's is space delimited, *UNKNOWN* first)
+  ASSUME
+    - coordinates for OOV come first
+    - dictionary is in inverse Zipf order (so overwrite less with more common)
+*/
+
+EigenDictionary
+make_eigen_dictionary(std::string filename, int nEigenDim, std::set<std::string> const& vocabulary)
+{
+  using std::string;
+  EigenDictionary  dictionary;
+  bool needToFlushEigenStream = false;
+  {
+    std::ifstream eigenStream (filename.c_str(), std::ifstream::in);
+    if (eigenStream.fail())
+    { std::cerr << "ERROR: Eigen dictionary file `" << filename << "' not found; terminating.\n";
+      return dictionary;
+    }
+    std::clog << "DICT: Build eigen dictionary with initial dim " << nEigenDim << " from file " << filename << std::endl;
+    { // special handling for first line, assumed to define OOV coordinates
+      std::vector<float> oov;
+      string token;
+      eigenStream >> token;  // toss file label for OOV; use "OOV"
+      string line;
+      std::getline(eigenStream, line);
+      std::istringstream ss(line);
+      float x;
+      while (ss >> x) oov.push_back(x);
+      if (nEigenDim == 0)
+      { nEigenDim = (int) oov.size();
+	if (verbose) std::clog << "Found eigen dimension = " << oov.size() << " based on found size for OOV.\n";
+	needToFlushEigenStream = false;
+      }
+      else
+      { if (nEigenDim < (int)oov.size())
+	{ oov.resize(nEigenDim);
+	  needToFlushEigenStream = true;
+	  if (verbose) std::clog << "       Will need to flush trailing elements in eigen stream.\n";
+	}
+	else
+	{ std::cerr << "ERROR: Found eigen dimension " << oov.size() << " which is smaller than requested " << nEigenDim << std::endl;
+	  return dictionary;
+	}
+      }
+      dictionary["OOV"] = oov;
+    }
+    while(!eigenStream.eof())
+    { string token;
+      string junk;
+      eigenStream >> token;
+      if(token.size() == 0) break;       // file has trailing blank line
+      std::transform(token.begin(), token.end(), token.begin(), ::tolower);
+      if(vocabulary.count(token) != 0)   // word type found in vocabulary
+      { std::vector<float> coor(nEigenDim);
+	for(int i=0; i<nEigenDim; ++i)
+	  eigenStream >> coor[i];
+	if (needToFlushEigenStream) std::getline(eigenStream, junk);
+	dictionary[token] = coor;        // over-writes if token present
+	if (verbose) std::clog << "      Added dictionary coor for token " << token << std::endl;
+      }
+      else // flush rest of line
+	std::getline(eigenStream, junk);
+    }
+  }
+  std::clog << "      Completed eigenword dictionary of size " << dictionary.size() << std::endl;
+  return dictionary;
+}
 
 void
 parse_arguments(int argc, char** argv,
