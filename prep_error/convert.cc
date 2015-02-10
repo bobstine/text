98ee5468ed66e1df6a1a9bc7c1d0779a169492e6
Bob Stine <stine@wharton.upenn.edu>
2015-02-10 16:03:18 -0500

    Can build auction data using NYT feed from Joel

--- prep_error/convert.cc
+++ prep_error/convert.cc
@@ -3,9 +3,9 @@
   that will then be processed to give word2vec coordinates.  Exports only those
   columns identified the tags file.
 
-  Joel style (space delimited)
-       into DY BGL#hurry_VBD BGR#the_ATI FFtag#, FFword#, FH#classroom_NN FHtag#NN FHword#classroom
-       around DY BGL#look_VBD BGR#the_ATI FFtag#IN FFword#for PV#look_VBD
+  Joel style (tab or space delimited)
+       into  BGL#hurry_VBD BGR#the_ATI FFtag#, FFword#, FH#classroom_NN FHtag#NN FHword#classroom
+       around BGL#look_VBD BGR#the_ATI FFtag#IN FFword#for PV#look_VBD
 
   Rectangular style (tab delimited, with uniform set of columns)
        Y        BGL          BGR       FFtag  FFword   FH            FHtag    FHword     PV
@@ -29,7 +29,7 @@
 const bool verbose = false;
 
 void
-parse_arguments(int argc, char** argv, bool &keepPOS, std::string& tagFileName);
+parse_arguments(int argc, char** argv, bool &embeddedPOS, bool &keepPOS, bool &removeDY, std::string& tagFileName);
 
 
 int main(int argc, char** argv)
@@ -40,8 +40,10 @@ int main(int argc, char** argv)
 
   // defaults
   string tagFileName ("tags.txt");
-  bool   keepPOS     (false);
-  parse_arguments(argc, argv, keepPOS, tagFileName);
+  bool   embeddedPOS (false);   // some files embed POS onto the word token
+  bool     keepPOS   (false);   
+  bool    removeDY   (false);   // a file had extra delimiter after prep
+  parse_arguments(argc, argv, embeddedPOS, keepPOS, removeDY, tagFileName);
 
   // read set of tags
   std::ifstream tagStream (tagFileName.c_str(), std::ifstream::in);
@@ -69,7 +71,8 @@ int main(int argc, char** argv)
   while (!std::cin.eof())
   { string thePrep;
     string dlTag;
-    std::cin >> thePrep >> dlTag;
+    std::cin >> thePrep;
+    if (removeDY) std::cin>> dlTag;
     string theLine;
     std::getline(std::cin, theLine);
     trim(theLine);
@@ -83,10 +86,10 @@ int main(int argc, char** argv)
 	token = trim(token);
 	std::size_t pos = token.find('#');
 	if(pos != string::npos)
-	{ string key    = token.substr(0,pos);
-	  if(tags.find(key) != tags.end())
+	{ string key    = token.substr(0,pos);   // trim off #, leaving column name
+	  if(tags.find(key) != tags.end())       // keep this tag and token
 	  { string value  = token.substr(pos+1);
-	    if(!keepPOS)
+	    if (embeddedPOS && (!keepPOS))       // words marked with POS in tag after _
 	    { std::size_t p = value.find('_');
 	      if(p != string::npos)
 		value = value.substr(0,p);
@@ -96,7 +99,7 @@ int main(int argc, char** argv)
 	  }
 	}
       }
-      // write the line
+      // write the line, tab delimited
       std::cout << thePrep;
       for (string tag : tags)
       { string value = tagValues[tag];
@@ -111,23 +114,28 @@ int main(int argc, char** argv)
 }
 
 void
-parse_arguments(int argc, char** argv, bool &keepPOS, std::string& tagFileName)
+parse_arguments(int argc, char** argv, bool &embeddedPOS, bool &keepPOS, bool &removeDY, std::string& tagFileName)
 {
   static struct option long_options[] = {
     //    {"risk",               no_argument, 0, 'R'},
+    {"embedded_POS", no_argument,       0, 'e'},
     {"keep_POS",     no_argument,       0, 'p'},
+    {"remove_DY",    no_argument,       0, 'r'},
     {"tag_file",     required_argument, 0, 't'},
     {0, 0, 0, 0}                             // terminator
   };
   int key;
   int option_index = 0;
-  while (-1 !=(key = getopt_long (argc, argv, "pt:", long_options, &option_index))) // colon means has argument
+  while (-1 !=(key = getopt_long (argc, argv, "eprt:", long_options, &option_index))) // colon means has argument
   {
     // std::cout << "PARSE: Key " << char(key) << " for option " << long_options[option_index].name << ", option_index=" << option_index << std::endl;
     switch (key)
     {
+    case 'e' :  { embeddedPOS = true; 	break;      }
     case 'p' :  { keepPOS = true; 	break;      }
+    case 'r' :  { removeDY = true; 	break;      }
     case 't' :  { tagFileName = optarg; break;      }
+
       /*    case 'n' :
       {
 	nRounds = read_utils::lexical_cast<int>(optarg);
